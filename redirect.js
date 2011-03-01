15279f478924b2694fb736c6fd8b76fea43ba570
Enio Lopes <eniolopes@gmail.com>
2011-03-01 01:28:05 -0300

    Added graph that shows the number of access to a link per date

--- redirect.js
+++ redirect.js
@@ -7,6 +7,8 @@ client.on("error", function (err) {
 
 var http = require('http'),
     express = require('express');
+require('datejs')
+
 
 var app = express.createServer();
 app.use(express.bodyDecoder());
@@ -17,7 +19,7 @@ var store = function(site, url, res)
     client.get(site, function(err, reply) {
         if (null == reply) {
             client.set(site, url);
-            client.set(toInfoShortcut(site), 0);
+            client.set(toInfoKey(site), 0);
             res.send(site + ' now contains ' + url + '.\n');
         } else {
             res.send(409);
@@ -32,6 +34,46 @@ var isInfoShortcut = function(site)
 
 var toInfoShortcut = function(site) {
     return isInfoShortcut(site) ? site : site + '+';
+};
+
+var toInfoKey = function(site) {
+    return site.replace('+', '')+'-'+Date.today().toString("d/M/yyyy")
+};
+
+var getKeysForGraph = function(site){
+	keys = [];
+	date = Date.parse('today');
+	for (var i=0; i < 7; i++) {
+		key = site.replace('+', '')+'-'+date.toString("d/M/yyyy");
+		keys.push(key);
+		date.add(-1).days();
+	};
+	return keys;
+};	
+
+var buildGraphUrl = function(keys, values){
+	if (values[0] == null) values[0] = 0
+	val = values[0];
+	data = keys[0].split('-')[1].split('/')
+	k = '|'+data[0]+'%2F'+data[1]				
+	for (var i=1; i < values.length; i++) {
+		if (values[i] != null){
+			val += ','+values[i]
+			data = keys[i].split('-')[1].split('/')
+			k += '|'+data[0]+'%2F'+data[1]			
+		}			
+	};
+
+	return 'http://chart.apis.google.com/chart?chxl=0:'+k+'&chxr=0,-3.333,100&chxt=x,y&chbh=23,15,10&chs=350x200&cht=bvs&chds=1.667,150&chd=t:'+val
+	
+};
+
+var showGraph = function(site, res) {
+	keys = getKeysForGraph(site);
+	client.mget(keys, function(err, reply){
+		graphUrl = buildGraphUrl(keys, reply);
+		res.redirect(graphUrl, 301);	
+	});
 };
 
 var validateSite = function(site, response)
@@ -60,12 +102,13 @@ app.get('/:site', function(req, res)
 {
    var site = req.params.site;
 
-    client.setnx(toInfoShortcut(site), 0);
+    client.setnx(toInfoKey(site), 0);
     client.get(site,function (err,reply){
-        if (isInfoShortcut(site)) {
-            res.send("The "+req.params.site+" has been accessed "+reply+" times.")
+        if (isInfoShortcut(site)) {			
+			showGraph(site, res)	
+            //res.send("The "+req.params.site+" has been accessed "+reply+" times.")			
         } else {
-            client.incr(toInfoShortcut(site));
+            client.incr(toInfoKey(site));
             res.redirect(reply, 301);
         }
     });
